9b4b77a952b0f4fad961829e2db78e5cc49b0ff1
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-05 08:20:46 -0300

    add estrutura para cadastro de medico

---
+++ pages/novo-medico.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Novo Médico
+      </h1>
+
+      <b-form @submit="enviar" @reset="resetar">
+        <b-form-group label="Nome Completo:" label-for="input-nome">
+          <b-form-input
+            id="input-nome"
+            v-model="medico.nome"
+            required
+            placeholder="Seu nome">
+
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Especialidade:" label-for="input-especialidade">
+          <b-form-select
+            id="inline-form-custom-select-pref"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            :options="especialidades"
+            :value="'Escolha sua especalidade'"
+          ></b-form-select>
+        </b-form-group>
+
+        <b-form-group label="Posso atender:" label-for="checkbox-dias-da-semana">
+          <b-form-checkbox-group
+            id="checkbox-dias-da-semana"
+            v-model="medico.diasDaSemana"
+            :options="diasDaSemana"
+            name="flavour-1"
+          ></b-form-checkbox-group>
+        </b-form-group>
+
+        <b-form-group label="Posso atender:" label-for="timepicker-inicio-atendimento">
+          <b-form-timepicker id="timepicker-inicio-atendimento" v-model="medico.inicioAtendimento"
+                             locale="en"></b-form-timepicker>
+        </b-form-group>
+
+        <b-form-group label="Posso atender:" label-for="timepicker-fim-atendimento">
+          <b-form-timepicker id="timepicker-fim-atendimento" v-model="medico.fimAtendimento"
+                             locale="en"></b-form-timepicker>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "novo-medico",
+  data() {
+    return {
+      medico: {
+        nome: '',
+        especialidade: [],
+        diasDaSemana: [],
+        inicioAtendimento: '',
+        fimAtendimento: ''
+      },
+      especialidades: [
+        {text: 'Clínica Geral', value: 'CG'},
+        {text: 'Ginecologia', value: 'GI'},
+        {text: 'Pediatria', value: 'PE'}
+      ],
+      diasDaSemana: [
+        {text: 'Segunda', value: 'segunda'},
+        {text: 'Terça', value: 'terca'},
+        {text: 'Quarta', value: 'quarta'},
+        {text: 'Quinta', value: 'quinta'},
+        {text: 'Sexta', value: 'sexta'},
+        {text: 'Sabádo', value: 'sabado'},
+        {text: 'Domingo', value: 'domingo'},
+      ]
+
+    }
+  },
+  methods: {
+    enviar() {
+
+    },
+    resetar() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+</style>
