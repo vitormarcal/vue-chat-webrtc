c9012bb78831933e7f251f41373455a6036260a6
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-07 10:12:32 -0300

    mudanças de estilo

---
+++ components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/tecnicos">Técnicos</b-nav-item>
+          <b-nav-item href="/agenda">Agendar</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <nav-bar></nav-bar>
+  </header>
+</template>
+
+<script>
+import NavBar from "../components/NavBar";
+export default {
+name: "Header",
+  components: {NavBar}
+}
+</script>
+
+<style scoped>
+
+header {
+  background-color: #17a2b8;
+  position: fixed;
+  min-height: 80px;
+  width: 100%;
+  z-index: 1000;
+  border-bottom: .0625rem solid rgba(0, 0, 0, .05);
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: .2375rem 2.5rem;
+  top: 0;
+}
+
+@media (max-width: 720px) {
+  header {
+    display: block;
+  }
+
+}
+
+
+</style>
