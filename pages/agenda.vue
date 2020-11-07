f1fb8cbf970e6b18da08db25c063c06769d14233
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-07 01:38:49 -0300

    criado agenda

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -2,6 +2,19 @@
   <div class="container">
 
     <div>
+
+      <div>
+        <h1 class="title">
+          Agenda para Teleatendimento
+        </h1>
+
+        <p class="subtitle">
+          Essa agenda é destinada para pessoas que desejam o atendimento on-line
+        </p>
+      </div>
+
+
+
       <selecao-especialidades :especialidade.sync="especialidade" @change="filtrar"></selecao-especialidades>
 
       <b-form-group label="Escolha o dia:">
@@ -27,33 +40,31 @@
             </template>
           </b-table>
         </div>
-        <div class="row">
+        <div class="row align-items-center">
 
-          <div>
-            <ul class="pagination">
-              <li class="paginate_button first disabled links" id="tblTabela_first"><a href="#" class="button--grey"
-                                                                                       aria-controls="tblTabela"
-                                                                                       data-dt-idx="0"
-                                                                                       tabindex="0">Primeiro</a>
-              </li>
-              <li class="paginate_button previous disabled links" id="tblTabela_previous"><a href="#"
-                                                                                             class="button--grey"
-                                                                                             aria-controls="tblTabela"
-                                                                                             data-dt-idx="1"
-                                                                                             tabindex="0">Anterior</a>
-              </li>
-              <li class="paginate_button next disabled links" id="tblTabela_next"><a href="#" class="button--grey"
+          <ul class="pagination ">
+            <li class="paginate_button first disabled links" id="tblTabela_first"><a href="#" class="button--grey"
                                                                                      aria-controls="tblTabela"
-                                                                                     data-dt-idx="2"
-                                                                                     tabindex="0">Próximo</a>
-              </li>
-              <li class="paginate_button last disabled links" id="tblTabela_last"><a href="#" class="button--grey"
-                                                                                     aria-controls="tblTabela"
-                                                                                     data-dt-idx="3"
-                                                                                     tabindex="0">Último</a>
-              </li>
-            </ul>
-          </div>
+                                                                                     data-dt-idx="0"
+                                                                                     tabindex="0">Primeiro</a>
+            </li>
+            <li class="paginate_button previous disabled links" id="tblTabela_previous"><a href="#"
+                                                                                           class="button--grey"
+                                                                                           aria-controls="tblTabela"
+                                                                                           data-dt-idx="1"
+                                                                                           tabindex="0">Anterior</a>
+            </li>
+            <li class="paginate_button next disabled links" id="tblTabela_next"><a href="#" class="button--grey"
+                                                                                   aria-controls="tblTabela"
+                                                                                   data-dt-idx="2"
+                                                                                   tabindex="0">Próximo</a>
+            </li>
+            <li class="paginate_button last disabled links" id="tblTabela_last"><a href="#" class="button--grey"
+                                                                                   aria-controls="tblTabela"
+                                                                                   data-dt-idx="3"
+                                                                                   tabindex="0">Último</a>
+            </li>
+          </ul>
         </div>
 
       </fieldset>
@@ -106,11 +117,40 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  padding: 5vh;
 }
 
 .links {
   padding-top: 15px;
   padding-left: 15px;
 }
+
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 40px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 16px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -112,10 +112,13 @@ export default {
     loggedIn() {
       return this.$store.state.auth.logado;
     },
+    user() {
+      return this.$store.state.auth.user;
+    },
   },
   created() {
     if (this.loggedIn) {
-      this.$router.push('/tecnicos');
+      this.$router.push('/agenda');
     }
   },
 }
@@ -129,6 +132,31 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
 }
 
 .links {
