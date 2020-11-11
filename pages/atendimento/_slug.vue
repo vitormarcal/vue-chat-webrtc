daecd5df16ea8629b03fee5a75b41fce4e393ea9
Vítor Marçal <git@vitormarcal.com.br>
2020-11-10 23:19:12 -0300

    conexão via ws

---
+++ pages/atendimento/_slug.vue
@@ -0,0 +1,111 @@
+<template>
+  <b-container class="container">
+    <div>
+      <h1 class="title">
+        Chat de Teleatendimento
+      </h1>
+
+      <p class="subtitle" v-show="this.consulta">
+        Você está no atendimento agendado para a especialidade {{ this.tituloChat }}
+      </p>
+
+      <chat :to="destinatario" :title="this.tituloChat" :from="this.usuarioCorrente"></chat>
+
+    </div>
+  </b-container>
+</template>
+
+<script>
+import Chat from "../../components/atendimento/Chat";
+import ConsultaService from "@/services/consulta.service"
+
+export default {
+  name: "index",
+  middleware: ['autenticado', 'cadastro-incompleto'],
+  components: {Chat},
+  data() {
+    return {
+      consulta: null,
+    }
+  },
+  async asyncData({params}) {
+    const id = params.slug;
+    return {id}
+  },
+  computed: {
+    usuarioCorrente() {
+      return this.$store.state.auth.user;
+    },
+    tituloChat() {
+      return this.consulta?.especialidade;
+    },
+    destinatario() {
+      if (this.usuarioCorrente.id === this.consulta?.idUsuarioTecnico) {
+        return {
+          id: this.consulta?.idUsuario,
+          nome: this.consulta?.nomeUsuario || this.consulta?.usernameUsuario
+        }
+      } else if (this.usuarioCorrente.id === this.consulta?.idUsuario) {
+        return {
+          id: this.consulta?.idUsuarioTecnico,
+          nome: this.consulta?.nomeTecnico || this.consulta?.usernameTecnico
+        }
+      } else {
+        return {}
+      }
+    }
+  },
+  mounted() {
+    ConsultaService.buscarConsulta(this.id)
+      .then(
+        consulta => {
+          this.consulta = consulta;
+        }
+      )
+      .catch(
+        error => {
+          let message = "Ocorreu um erro";
+          if (error?.response?.data?.message) {
+            message = error.response.data.message
+          } else if (error?.message) {
+            message = error.message;
+          }
+          this.$bvToast.toast(message, {
+            title: 'Busca da consulta:',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      )
+  }
+}
+</script>
+
+<style scoped>
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 32px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 1rem;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+</style>
--- services/consulta.service.js
+++ services/consulta.service.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import authHeader from "./auth.header";
 
-const API_URL = `${process.env.backendApi}/consultas/disponiveis`;
+const API_URL = `${process.env.backendApi}/consultas`;
 
 class ConsultaService {
   buscarTodasDisponiveis(filtro) {
     const data = filtro?.data || '';
     const idEspecialidade = filtro?.idEspecialidade || '';
 
-    let url = `${API_URL}?data=${data}&idEspecialidade=${idEspecialidade}`;
+    let url = `${API_URL}/disponiveis?data=${data}&idEspecialidade=${idEspecialidade}`;
 
     return axios
       .get(url,
@@ -21,11 +21,22 @@ class ConsultaService {
 
   agendarParaUsuario(idConsulta) {
 
-    let url = `${API_URL}/${idConsulta}`;
+    let url = `${API_URL}/disponiveis/${idConsulta}`;
 
     return axios
       .put(url,
         {},
+        {headers: authHeader()}
+      )
+      .then(response => {
+        return response.data;
+      });
+  }
+
+  buscarConsulta(idConsulta) {
+    let url = `${API_URL}/${idConsulta}`;
+    return axios
+      .get(url ,
         {headers: authHeader()}
       )
       .then(response => {
