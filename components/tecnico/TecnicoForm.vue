991ebce072b4ae7366aad430d43e89e80f32203c
Vítor Marçal <git@vitormarcal.com.br>
2020-11-13 09:46:15 -0300

    refatorado tecnico form

--- components/tecnico/TecnicoForm.vue
+++ components/tecnico/TecnicoForm.vue
@@ -7,23 +7,80 @@
       @{{ usuarioCorrente.username }}, complete seu cadastro.
     </h1>
 
+    <div>
+      <selecao-especialidades :especialidade.sync="tecnico.idEspecialidade"></selecao-especialidades>
 
-    <Tecnico :tecnico="tecnico"/>
+      <b-form-group label="Atendo na:" label-for="checkbox-dias-da-semana">
+        <b-form-checkbox-group
+          id="checkbox-dias-da-semana"
+          v-model="tecnico.diasQueAtende"
+          :options="diasDaSemana"
+          name="flavour-1"
+        ></b-form-checkbox-group>
+      </b-form-group>
 
-    <b-button type="submit" variant="primary" v-if="editar" @click="editarTecnico">Editar</b-button>
-    <b-button type="submit" variant="primary" v-else @click="salvarTecnico">Cadastrar</b-button>
-    <b-button type="reset" variant="danger" @click="resetar">Limpar</b-button>
+      <b-form-group label="Meus atendimentos duram (min):" label-for="duracaoAtendimento">
+        <b-form-input type="number" id="duracaoAtendimento"
+                      :state="duracaoAtendimentoState"
+                      v-model="tecnico.duracaoAtendimento"
+                      locale="pt-BR"></b-form-input>
+
+      </b-form-group>
+
+      <b-form-group label="Inicio os trabalhos às:" label-for="inicioAtendimento">
+        <b-form-timepicker id="inicioAtendimento" v-model="tecnico.inicioAtendimento"
+                           :label-close-button="'Fechar'"
+                           :label-no-time-selected="'Selecione um horário'"
+                           locale="pt-BR"></b-form-timepicker>
+      </b-form-group>
+      <b-form-group label="Saio para descanso às:" label-for="saidaDescanso">
+        <b-form-timepicker id="saidaDescanso" v-model="tecnico.saidaDescanso"
+                           :label-close-button="'Fechar'"
+                           :label-no-time-selected="'Selecione um horário'"
+                           locale="pt-BR"></b-form-timepicker>
+      </b-form-group>
+      <b-form-group label="Volto do descanso às:" label-for="voltaDescanso">
+        <b-form-timepicker id="voltaDescanso" v-model="tecnico.voltaDescanso"
+                           :label-close-button="'Fechar'"
+                           :label-no-time-selected="'Selecione um horário'"
+                           locale="pt-BR"></b-form-timepicker>
+      </b-form-group>
+      <b-form-group label="Finalizo o expediente  às:" label-for="fimAtendimento">
+        <b-form-timepicker id="fimAtendimento" v-model="tecnico.fimAtendimento"
+                           :label-close-button="'Fechar'"
+                           :label-no-time-selected="'Selecione um horário'"
+                           locale="pt-BR"></b-form-timepicker>
+
+      </b-form-group>
+
+      <b-button type="submit" variant="primary" v-if="editar" @click="editarTecnico">Editar</b-button>
+      <b-button type="submit" variant="primary" v-else @click="salvarTecnico">Cadastrar</b-button>
+      <b-button type="reset" variant="danger" @click="resetar">Limpar</b-button>
+    </div>
   </div>
 </template>
 
 <script>
-import Tecnico from "@/components/tecnico/Tecnico";
 import TecnicoModel from "@/components/tecnico/tecnico.model.js";
+import SelecaoEspecialidades from "./SelecaoEspecialidades";
 
 export default {
   name: "TecnicoForm",
-  components: {Tecnico},
+  components: {SelecaoEspecialidades},
   props: ['editar', "tecnico"],
+  data() {
+    return {
+      diasDaSemana: [
+        {text: 'Segunda', value: 'SEGUNDA'},
+        {text: 'Terça', value: 'TERCA'},
+        {text: 'Quarta', value: 'QUARTA'},
+        {text: 'Quinta', value: 'QUINTA'},
+        {text: 'Sexta', value: 'SEXTA'},
+        {text: 'Sabádo', value: 'SABADO'},
+        {text: 'Domingo', value: 'DOMINGO'},
+      ],
+    }
+  },
   methods: {
     salvarTecnico() {
 
@@ -106,6 +163,12 @@ export default {
     }
   },
   computed: {
+    duracaoAtendimentoState() {
+      if (this.tecnico.duracaoAtendimento == null) {
+        return null
+      }
+      return this.tecnico.duracaoAtendimento > 5
+    },
     accessToken() {
       return this.$store.state.auth.accessToken;
     },
