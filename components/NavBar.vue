d60d39ee44b22dcc003baf62c17fe4f6da0f098f
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-20 18:04:24 -0300

    Revert "add teste de camera"
    This reverts commit 19468ca7

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -53,11 +53,6 @@ export default {
           label: 'Minhas marcações',
           active: this.logado
         },
-        {
-          link: '/tecnicos/outra/',
-          label: 'Outra',
-          active: this.logado
-        },
       ];
     }
   }
