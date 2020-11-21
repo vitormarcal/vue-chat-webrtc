9e92af092d436af7ecce7550a1beb7985567359b
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-21 09:48:55 -0300

    add dependecies para video

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,11 @@
+import colors from 'vuetify/es5/util/colors'
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
   env: {
     backendApi: process.env.BACKEND_API || 'http://localhost:8080',
+    WS_URL: process.env.WS_URL || 'https://videocall-rtc.herokuapp.com'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
@@ -39,7 +41,29 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     '@nuxtjs/axios',
+    '@nuxtjs/vuetify',
   ],
+/*
+** vuetify module configuration
+** https://github.com/nuxt-community/vuetify-module
+*/
+vuetify: {
+  customVariables: ['~/assets/variables.scss'],
+    theme: {
+    dark: false,
+      themes: {
+      dark: {
+        primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+      }
+    }
+  }
+},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
