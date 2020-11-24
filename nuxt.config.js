2c05cfe0a3a8f5af202d4d464c33b4d52595a24d
Vítor Marçal <git@vitormarcal.com.br>
2020-11-24 19:33:56 -0300

    add env variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   env: {
     backendApi: process.env.BACKEND_API || 'http://localhost:8080',
-    WS_URL: 'http://localhost:3000' || 'https://videocall-rtc.herokuapp.com'
+    WS_URL: process.env.CHAT_API || 'http://localhost:3000',
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
